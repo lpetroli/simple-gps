ba7a3fd7e65bd23661c16527945de6a25da2d390
Lorenzo Petroli <lpetroli@gmail.com>
2014-08-15 03:51:03 -0300

    Add basic funcionality to EditText and TextView elements on MainActivity

--- app/src/main/java/com/lpetroli/simplegps/MainActivity.java
+++ app/src/main/java/com/lpetroli/simplegps/MainActivity.java
@@ -2,14 +2,23 @@ package com.lpetroli.simplegps;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private static final String LOG_TAG = "MainActivity";
+
+    private EditText mPlaceEditText;
+    private TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +30,6 @@ public class MainActivity extends Activity {
                     .commit();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,6 +51,21 @@ public class MainActivity extends Activity {
     }
 
     /**
+     * Search the weather of the place referenced by {@code R.id.edit_text_place}
+     *
+     * @param view a reference to current View
+     */
+    public void searchWeather(View view) {
+        mPlaceEditText = (EditText) findViewById(R.id.edit_text_place);
+        mWeatherTextView = (TextView) findViewById(R.id.text_view_weather);
+
+        String place = mPlaceEditText.getText().toString();
+        Log.d(LOG_TAG, "Show weather from: " + place);
+
+        mWeatherTextView.setText(place);
+    }
+
+    /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
@@ -54,8 +77,6 @@ public class MainActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-
 
             return rootView;
         }
