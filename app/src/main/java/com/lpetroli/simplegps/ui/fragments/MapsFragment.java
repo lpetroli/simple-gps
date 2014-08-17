515a30f3370592a4690e60696725b960b8c8a22e
Lorenzo Petroli <lpetroli@gmail.com>
2014-08-17 07:10:01 -0300

    Acquire a valid map reference. Add a marker on the map for devices current location.

--- app/src/main/java/com/lpetroli/simplegps/ui/fragments/MapsFragment.java
+++ app/src/main/java/com/lpetroli/simplegps/ui/fragments/MapsFragment.java
@@ -1,57 +1,120 @@
 package com.lpetroli.simplegps.ui.fragments;
 
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.lpetroli.simplegps.R;
 
-public class MapsFragment extends MapFragment {
+public class MapsFragment extends MapFragment implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener {
+
     private static final String LOG_TAG = "MapsFragment";
 
-    private static final int MAX_GET_MAP_RETRIES = 5;
-    private static final long WAIT_MILLS = 500;
+    /*
+     * Define a request code to send to Google Play services
+     * This code is returned in Activity.onActivityResult
+     */
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     private GoogleMap mMap;
+    private LocationClient mLocationClient;
 
     public MapsFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-        /* This background task is intended to acquire a valid GoogleMap reference, taking into
-         * account delays that may exist on map availability during fragment creation
-         */
-        Runnable background = new Runnable() {
-            @Override
-            public void run() {
-                MapFragment mFragment = MapFragment.newInstance();
-                for(int retries = 0; retries < MAX_GET_MAP_RETRIES; retries++){
-                    if((mMap = mFragment.getMap()) != null) {
-                        break;
-                    }
-                    try {
-                        Thread.sleep(WAIT_MILLS, 0);
-                    } catch (InterruptedException e) {
-                        Log.e(LOG_TAG, "Error: ", e);
-                    }
-                }
-
-                if(mMap == null) {
-                    Log.d(LOG_TAG, getString(R.string.message_no_weather));
-                }
-            }
-        };
-
-        new Thread( background ).start();
+        if(servicesConnected()) {
+            mMap = getMap();
+            mLocationClient = new LocationClient(getActivity(), this, this);
+        } else {
+            Log.d(LOG_TAG, getString(R.string.message_no_weather));
+        }
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
+    public void onStart() {
+        super.onStart();
+        mLocationClient.connect();
+    }
+    @Override
+    public void onStop() {
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+
+        setUpMapLocation();
+    }
+
+    @Override
+    public void onDisconnected() {
+        // do nothing
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(getActivity(),
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            } catch (IntentSender.SendIntentException e) {
+                Log.e(LOG_TAG, "Error: ", e);
+            }
+        } else {
+            // Get the error dialog from Google Play services
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), getActivity(),
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST).show();
+        }
+    }
+
+    private void setUpMapLocation() {
+        Location currentLocation;
+        LatLng coordinates;
+        MarkerOptions customMarker;
+
+        currentLocation = mLocationClient.getLastLocation();
+        coordinates = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        customMarker = new MarkerOptions();
+        customMarker.position(coordinates);
+
+        mMap.addMarker(customMarker);
+    }
+
+    private boolean servicesConnected() {
+        // Check if Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+        if (ConnectionResult.SUCCESS == resultCode) {
+            return true;
+        }
+
+        // Get the error dialog from Google Play services
+        GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(),
+                CONNECTION_FAILURE_RESOLUTION_REQUEST).show();
+        return false;
     }
 }
